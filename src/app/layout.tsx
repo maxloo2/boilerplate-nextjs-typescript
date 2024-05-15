6979b1af5897a9c142abcf77a490aba522594eac
maxloo2 <4709356+maxloo2@users.noreply.github.com>
2024-05-16 03:24:43 +0800

    refactor: v1.0.1 - remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './styles.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'boilerplate-nextjs-typescript',
